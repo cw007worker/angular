40835ce98bcc70c828a6b21507f477ab47a5f74b
linwinpei <52321057+linwinpei@users.noreply.github.com>
2019-08-22 20:03:03 +0800

    Reactive form created

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from './../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -8,10 +9,23 @@ import { CartService } from './../cart.service';
 })
 export class CartComponent implements OnInit {
   iterms;
-  constructor(private cartService: CartService) {
+  checkoutForm;
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
+  ) {
     this.iterms = this.cartService.getItems();
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
   }
-
+  onSubmit(customerData) {
+    //Process checkout data here..
+    console.warn('Your order has been submitted', customerData);
+    this.iterms = this.cartService.clearCart();
+    this.checkoutForm.reset();
+  }
   ngOnInit() {
   }
 
